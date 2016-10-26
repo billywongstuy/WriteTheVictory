5f0c6cacdccf70a17db0e8379af764f9a8f7c071
billywongstuy <bwong5@stuy.edu>
2016-10-26 15:23:47 -0400

    Finished the assignment

---
+++ DEVISE.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main() {
+  srand(time(NULL));
+  int a1[10];
+  int a2[10];
+  int i = 0;
+  int fd;
+  printf("Generating random numbers:\n");
+  for (i; i < 10; i++) {
+    int r = rand();
+    a1[i] = r;
+    printf("\trandom %d: %d\n",i,r);
+  }
+  
+  umask(0000);
+  //CREATE THE FILE
+  fd = open("file.txt",O_CREAT,0666);
+  close(fd);
+
+  //WRITE TO THE FILE
+  fd = open("file.txt",O_RDWR);
+  printf("Writing numbers to file...\n");
+  write(fd,a1,sizeof(a1));
+  close(fd);
+
+  //READ THE FILE
+  fd = open("file.txt",O_RDWR);
+  printf("Reading numbers from file...\n");
+  read(fd,a2,sizeof(a2));
+  close(fd);
+
+  //CHECK THE ARRAY
+  printf("Verification that written values were the same:\n");
+
+  for (i = 0; i < 10; i++) {
+    printf("\trandom %d: %d\n",i,a2[i]);
+  }
+
+  return 0;
+}
